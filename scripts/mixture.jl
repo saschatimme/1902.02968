a827a185c9466b93e5480f445f1768d47b31b6c2
Sascha Timme <sascha@timme.xyz>
2020-08-18 13:01:51 +0200

    Update mixture script

--- scripts/mixture.jl
+++ scripts/mixture.jl
@@ -11,30 +11,30 @@ include(joinpath(@__DIR__, "utils.jl"))
 using LinearAlgebra, DataFrames, CSV, Statistics, Printf, PrettyTables
 
 mixture_system = let
-    λ = Variable(:λ)
-    μ₁ = Variable(:μ₁)
-    μ₂ = Variable(:μ₂)
-    s₁ = Variable(:s₁)
-    s₂ = Variable(:s₂)
-    m₁ = Variable(:m₁)
-    m₂ = Variable(:m₂)
-    m₃ = Variable(:m₃)
-    m₄ = Variable(:m₄)
-    m₅ = Variable(:m₅)
+    lambda = Variable(:lambda)
+    mu1 = Variable(:mu1)
+    mu2 = Variable(:mu2)
+    s1 = Variable(:s1)
+    s2 = Variable(:s2)
+    m1 = Variable(:m1)
+    m2 = Variable(:m2)
+    m3 = Variable(:m3)
+    m4 = Variable(:m4)
+    m5 = Variable(:m5)
 
     eqs = [
-        -m₁ + λ * μ₁ + (1 - λ) * μ₂,
-        -m₂ + λ * (s₁ + (1 / 2) * μ₁^2) + (1 - λ) * (s₂ + (1 / 2) * μ₂^2),
-        -m₃ + λ * (s₁ * μ₁ + (1 / 6) * μ₁^3) + (1 - λ) * (s₂ * μ₂ + (1 / 6) * μ₂^3),
-        -m₄ +
-        λ * ((1 / 2) * s₁ * μ₁^2 + (1 / 2) * s₁^2 + (1 / 24) * μ₁^4) +
-        (1 - λ) * ((1 / 2) * s₂ * μ₂^2 + (1 / 2) * s₂^2 + (1 / 24) * μ₂^4),
-        -m₅ +
-        λ * ((1 / 6) * s₁ * μ₁^3 + (1 / 2) * s₁^2 * μ₁ + (1 / 120) * μ₁^5) +
-        (1 - λ) * ((1 / 6) * s₂ * μ₂^3 + (1 / 2) * s₂^2 * μ₂ + (1 / 120) * μ₂^5),
+        -m1 + lambda * mu1 + (1 - lambda) * mu2,
+        -m2 + lambda * (s1 + (1 / 2) * mu1^2) + (1 - lambda) * (s2 + (1 / 2) * mu2^2),
+        -m3 + lambda * (s1 * mu1 + (1 / 6) * mu1^3) + (1 - lambda) * (s2 * mu2 + (1 / 6) * mu2^3),
+        -m4 +
+        lambda * ((1 / 2) * s1 * mu1^2 + (1 / 2) * s1^2 + (1 / 24) * mu1^4) +
+        (1 - lambda) * ((1 / 2) * s2 * mu2^2 + (1 / 2) * s2^2 + (1 / 24) * mu2^4),
+        -m5 +
+        lambda * ((1 / 6) * s1 * mu1^3 + (1 / 2) * s1^2 * mu1 + (1 / 120) * mu1^5) +
+        (1 - lambda) * ((1 / 6) * s2 * mu2^3 + (1 / 2) * s2^2 * mu2 + (1 / 120) * mu2^5),
     ]
 
-    System(eqs, [λ, μ₁, s₁, μ₂, s₂], [m₁, m₂, m₃, m₄, m₅])
+    System(eqs, [lambda, mu1, s1, mu2, s2], [m1, m2, m3, m4, m5])
 end
 
 data_dir = joinpath(@__DIR__, "../data/mixture/")
@@ -43,16 +43,15 @@ p = read_parameters_file(data_dir * "start_parameters")
 
 
 df = DataFrame(accepted_steps = Int[],
-            rejected_steps = Int[], nΔs₁ = Int[], nΔs₂ = Int[])
+            rejected_steps = Int[], nΔs1 = Int[], nΔs2 = Int[])
 for i in 1:50
     q = read_parameters_file(data_dir * "target_parameters_$i")
     tracker = Tracker(ParameterHomotopy(mixture_system, p, q))
     for s in S
         r = track(tracker, s, 1.0, 0.0)
-        push!(df, (r.accepted_steps, r.rejected_steps, r.nΔs₁, r.nΔs₂))
+        push!(df, (r.accepted_steps, r.rejected_steps, r.nΔs1, r.nΔs2))
     end
 end
-
 
 table_data = let
     reshape(
@@ -61,8 +60,8 @@ table_data = let
             @sprintf("%.2f", mean(df.accepted_steps)),
             @sprintf("%.2f", mean(df.rejected_steps)),
             @sprintf("%.2f", mean(df.accepted_steps ./ (df.accepted_steps .+ df.rejected_steps))),
-            @sprintf("%.2f", mean(df.nΔs₁ ./ (df.accepted_steps .+ df.rejected_steps))),
-            @sprintf("%.2f", mean(df.nΔs₂ ./ (df.accepted_steps .+ df.rejected_steps))),
+            @sprintf("%.2f", 100 .* mean(df.nΔs1 ./ (df.accepted_steps .+ df.rejected_steps))),
+            @sprintf("%.2f", 100 - 100 .* mean(df.nΔs1 ./ (df.accepted_steps .+ df.rejected_steps))),
         ],
         1,
        6,
@@ -71,7 +70,7 @@ end
 
 
 header = [
-    "avg. steps" "avg. steps acc." "avg. steps rej." "% acc." "% 1" "% 2"
+    "avg. steps" "avg. steps acc." "avg. steps rej." "% acc." "% Δt₁" "% Δt₂"
 ]
 
-pretty_table( table_data, header)
+pretty_table(table_data, header)
